42f52656f321c6ab4d951585565bac8d97e72d7a
Adam P <106110648+adplotzk@users.noreply.github.com>
2023-05-05 17:48:29 -0400

    Update checksum.py

--- validate_1.0.0-rc.2/checksum.py
+++ validate_1.0.0-rc.2/checksum.py
@@ -4,7 +4,8 @@
  
 import hashlib
 import os
- 
+import shutil
+
 def get_checksum(filename, hash_function):
     """Generate checksum for file baed on hash function (MD5 or SHA256).
  
@@ -31,7 +32,7 @@ def get_checksum(filename, hash_function):
     return readable_hash
 
 hashcode = "checksum/hash.tar.gz"
-hashtest = "462000e7b041f54b1b8e4ef59e4372f62f9bb2d293c3f9122a4f19c46edadf17"
+hashtest = "59869db34853933b239f1e2219cf7d431da006aa919635478511fabbfc8849d2"
 
 sha256_result = get_checksum(hashcode, "sha256")
 #os.system("shasum -a 256 {}".format(hashcode))
@@ -48,3 +49,5 @@ if str(sha256_result) == str(hashtest):
 else:
     print("Checksums are not equal.")
     print("STATUS: ERROR DETECTED")
+
+shutil.rmtree("checksum")
